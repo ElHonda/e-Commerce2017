e005d2aa716594376c48faeb5b6b62c1df3359b5
ernan <ernan@ERNANE-NOTE>
2017-09-20 16:26:53 -0300

    EHR - Criação de DAO para Dimensão

--- e-Commerce2017-core/src/eCommerce/core/impl/dao/DimensaoDAO.java
+++ e-Commerce2017-core/src/eCommerce/core/impl/dao/DimensaoDAO.java
@@ -2,9 +2,15 @@ package eCommerce.core.impl.dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import eCommerce.core.utils.SqlBuilder;
 import eCommerce.dominio.Dimensao;
 import eCommerce.dominio.EntidadeDominio;
 
@@ -45,7 +51,65 @@ public class DimensaoDAO extends AbstractJdbcDAO{
 
 	@Override
 	public List<EntidadeDominio> consultar(EntidadeDominio entidade) throws SQLException {
-		// TODO Auto-generated method stub
+		PreparedStatement pst = null;
+		Dimensao dimensao = (Dimensao)entidade;
+		SqlBuilder sb = new SqlBuilder(this.table, colunas);
+	    Map<Integer,Object> hsWhere = new HashMap<Integer,Object>();
+		Integer lni = 1;
+
+		try {
+			openConnection();
+			
+		    if ( dimensao != null ){
+		    	if ( dimensao.getId() != null && dimensao.getId() > 0 ) {
+		    		sb.addWhere("id = ?" );
+		    		hsWhere.put(lni, dimensao.getId());
+		    		lni++;
+		    	}
+		    	
+		    	if ( dimensao.getDimensionavel() != null && dimensao.getDimensionavel().getId() != null && dimensao.getDimensionavel().getId() > 0 ) {
+		    		sb.addWhere("dimensionavel_id = ?");
+		    		hsWhere.put(lni, dimensao.getDimensionavel().getId() );
+		    		lni++;
+		    		sb.addWhere("dimensaionavel_class = ?");
+		    		hsWhere.put( lni , dimensao.getDimensionavel().getClass().getName() );
+		    		lni++;
+		    	}
+			}
+	    
+			pst = connection.prepareStatement(sb.getQuery(null));
+		
+		    for (Map.Entry<Integer, Object> entry : hsWhere.entrySet())
+		    {
+		    	pst.setObject(entry.getKey(), entry.getValue());
+		    }
+			
+			ResultSet rs = pst.executeQuery();
+			List<EntidadeDominio> dimensoes = new ArrayList<EntidadeDominio>();
+			while (rs.next()) {
+				Dimensao d = new Dimensao();
+				d.setId(rs.getInt("id"));
+				d.setAltura(rs.getDouble("altura"));
+				d.setLargura(rs.getDouble("largura"));
+				d.setPeso(rs.getDouble("peso"));
+				d.setProfundidade(rs.getDouble("profundidade"));
+
+			    EntidadeDominio entity = (EntidadeDominio)Class.forName(rs.getString("dimensionavel_class")).newInstance();
+			    entity.setId(rs.getInt("dimensionavel_id"));
+			    
+			    d.setDimensionavel(entity);
+				java.sql.Date dtCadastroEmLong = rs.getDate("dtcadastro");
+				if( dtCadastroEmLong != null ) {
+					Date dtCadastro = new Date(dtCadastroEmLong.getTime());				
+					d.setDtCadastro(dtCadastro);
+				}
+
+				dimensoes.add(d);
+			}
+			return dimensoes;
+		} catch (SQLException | InstantiationException | IllegalAccessException | ClassNotFoundException e) {
+			e.printStackTrace();
+		}
 		return null;
 	}
 
